f1889e2e82dbf7fae0d2972e3e96ba42754872df
Zach Willis <zachmw27@gmail.com>
2023-11-12 23:17:35 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    console.log(customerName.toUpperCase());
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'mike';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'diana';
+}
